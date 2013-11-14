eecfe562dda6027a21d63499b9f331be0621743a
Andrea Gottardi <andreagot16@gmail.com>
2013-11-14 13:20:48 +0100

    Finally it works (some things needs to be fixed in DB though.)

--- SciGot-DiscussionGroups/src/java/servlet/LoginServlet.java
+++ SciGot-DiscussionGroups/src/java/servlet/LoginServlet.java
@@ -10,8 +10,6 @@ import db.User;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
